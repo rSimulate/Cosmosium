0c84ee8a125fae70fcea98b5766fc5e1a56f7c62
Tylar Murray <murray.tylar@gmail.com>
2014-04-12 10:40:57 -0400

    added simple python bottle at /asteroidReq to test

---
+++ python/getAsteroid.py
@@ -0,0 +1,25 @@
+
+import requests
+from bottle import route, run
+
+@route('/asteroidReq')
+def processReq():
+    return '{"name:":"Hello World!"}'
+
+run(host='localhost', port=8080, debug=True)
+
+def byName(name):
+    raise NotImplementedError("getAsteroidByName not ready")
+    
+def asterankAPI(query, limit):
+    # queries the asterank API as guided by http://www.asterank.com/mpc
+    # query = (regex?) specifier string
+    # limit = max asteroids to return
+    payload = {'query':str(query),'limit':str(limit)}
+    r = requests.get("http://asterank.com/api/asterank", params=payload)
+    #print(r.url)
+    return r.json()
+
+if __name__ == "__main__":
+    resp = asterankAPI('{"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}}',2)
+    print resp
