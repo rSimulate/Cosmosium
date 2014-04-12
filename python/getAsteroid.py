a506a098a24a6385539d632987aed3cb2edf6011
Tylar Murray <murray.tylar@gmail.com>
2014-04-12 11:57:00 -0400

    added methods to add asteroid to list of tracked asteroids

--- python/getAsteroid.py
+++ python/getAsteroid.py
@@ -14,6 +14,14 @@ def asterankAPI(query, limit):
     print('json='+resp)
 #    print('text='+r.text)
     return resp
+    
+@route('/addAsteroid')
+def addOOI():
+    name = request.query.name
+    OOIs.append(byName(name))
+    print 'object '+name+' added to OOIs'
+    print OOIs
+
 
 @route('/asteroidReq')
 def processReq():
@@ -23,9 +31,11 @@ def processReq():
     return template('type: {{datatype}} (response {{res}})', datatype="asterank", res=asterankAPI(q,lim))
 
 def byName(name):
-    raise NotImplementedError("getAsteroidByName not ready")
+    q = '{"full_name":'+name+'}'
+    return asterankAPI(q,1)
 
 if __name__ == "__main__":
 #    resp = asterankAPI('{"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}}', 2)
 #    print resp
+    OOIs = list()
     run(host='localhost', port=8080, debug=True)
