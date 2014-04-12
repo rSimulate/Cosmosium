a69d0a4be5b6d3895e56a566d01708d843b80141
Tylar Murray <murray.tylar@gmail.com>
2014-04-12 05:38:01 -0400

    those files should have never been committed. its getting late.

--- js/MPO_data/MPO_req - Copy.js
+++
@@ -1,6 +0,0 @@
-// MPO request object test
-console.log("=MPO REQ TEST=")
-
-var req = new MPO_req(MPO_class.MAIN_BELT);
-console.log(req.send());
-
--- js/MPO_data/MPO_test.js
+++
@@ -1,8 +0,0 @@
-require("./MPO.js", function(){
-
-    console.log("=MPO TEST=");
-
-    var m = new MPO("Beerfest");
-    console.log(m.name);
-    console.log(m.getLoc(0));
-});
