c2e24b7d53dac3b982ee7ba3d084c0da29e5aaff
Tylar Murray <murray.tylar@gmail.com>
2014-04-14 18:56:59 -0400

    changed to port 80 for prod env

--- app.py
+++ app.py
@@ -247,5 +247,5 @@ def login_success():
 #=====================================#
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7099))
+    port = int(os.environ.get("PORT", 80))
     run(host='0.0.0.0', port=port)
