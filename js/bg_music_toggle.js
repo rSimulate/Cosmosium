0e9706326b26481986ca313a5859194bfe1002ea
Tylar Murray <murray.tylar@gmail.com>
2014-04-22 21:02:38 -0400

    fixed music text color changes when muted

--- js/bg_music_toggle.js
+++ js/bg_music_toggle.js
@@ -5,11 +5,10 @@ document.getElementById('musicMute').addEventListener('click', function (e)
     e = e || window.event;
     audio.muted = !audio.muted;
     
-    // NOTE: this doesn't work! Why?
     if (audio.muted){
-        document.getElementById('musicAttribution').color = "rgb(200,200,200)"
+        document.getElementById('musicAttribution').style.color = "rgb(200,200,200)"
     } else {
-        document.getElementById('musicAttribution').color = "rgb(100,100,100)"
+        document.getElementById('musicAttribution').style.color = "rgb(100,100,100)"
     }
     e.preventDefault();
 }, false);
