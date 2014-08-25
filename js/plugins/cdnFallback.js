387af2f4423004b73c08019264864627ef70c2de
BrianErikson <briankerikson@gmail.com>
2014-08-25 19:23:37 +0200

    Added jquery UI as an unimplemented fallback

--- js/plugins/cdnFallback.js
+++ js/plugins/cdnFallback.js
@@ -1,2 +1,3 @@
 window.jQuery || document.write("<script src='js/lib/techtreejs/js/lib/jquery-1.9.1.min.js'>\x3C/script>");
+$('body').hasOwnProperty('datepicker') || document.write("<script src='js/lib/plugins/jquery-ui.min.js'>\x3C/script>");
 //$('body').hasOwnProperty('off') || document.write("<script src='js/lib/plugins/bootstrap.js'>\x3C/script>");
