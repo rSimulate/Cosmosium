ff0f4e9dd0d86f5851c197a6d0a9e996fa063fe3
BrianErikson <briankerikson@gmail.com>
2014-08-25 11:50:06 +0200

    Added unimplemented start of cdnFallback mechanism

---
+++ js/plugins/cdnFallback.js
@@ -0,0 +1,2 @@
+window.jQuery || document.write("<script src='js/lib/techtreejs/js/lib/jquery-1.9.1.min.js'>\x3C/script>");
+//$('body').hasOwnProperty('off') || document.write("<script src='js/lib/plugins/bootstrap.js'>\x3C/script>");
