a0bb3c1576bb8af228c6f3a9b2e085105ee1c678
martin <kronberg.martin@gmail.com>
2014-08-26 22:00:15 -0700

    porkchops yo

--- py/TrajTest.py
+++ py/TrajTest.py
@@ -7,7 +7,7 @@ __author__ = 'martin'
 '''
 
 
-from lib.PyKEP.getTraj_simple import getTraj_simple
+from lib.PyKEP.C3 import planet_asteroid,planet_planet
 from PyKEP import epoch, DAY2SEC, planet_ss, AU, MU_SUN, lambert_problem, planet
 import AsteroidDB
 import numpy as np
@@ -16,20 +16,27 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import BoundaryNorm
 
-low_arrive=2250.0
-high_arrive=4000.0
+low_arrive=2185.0
+high_arrive=3500.0
 
-low_launch=2000.0
-high_launch=2200.0
+low_launch=1950.0
+high_launch=2162.0
 
-reso=50
+reso=100
 
 
 xy = np.zeros((reso,reso))
 
 
-
+rev=0
 
 for i in range(0,reso,1):
 
@@ -47,12 +54,19 @@ for i in range(0,reso,1):
             start_planet='earth'
             arrive_planet='mars'
 
+
             N=20
 
-            traj=getTraj_simple(start_planet,arrive_planet,tlaunch,tarrive,N)
+            traj=planet_asteroid(start_planet,tlaunch,tarrive,rev,N)
+
 
 
             xy[i,j]=traj
+
+
+
+
+
 
 
 #fig = plt.figure()
@@ -68,20 +82,47 @@ for i in range(0,reso,1):
 
 #plt.show()
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-X = np.arange(low_arrive, high_arrive,((high_arrive-low_arrive)/reso))
-Y = np.arange(low_launch, high_launch,((high_launch-low_launch)/reso))
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+Y = np.arange(low_arrive, high_arrive,((high_arrive-low_arrive)/reso))
+X = np.arange(low_launch, high_launch,((high_launch-low_launch)/reso))
 X, Y = np.meshgrid(X, Y)
 
-Z = xy
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-#ax.set_zlim(0, 5000)
 
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+norm=np.max(xy)
 
-fig.colorbar(surf, shrink=0.5, aspect=5)
+Z = xy/norm
+# surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+#         linewidth=0, antialiased=False)#,vmax=5E8)
+# #ax.set_zlim(0, 100)
+#
+# ax.zaxis.set_major_locator(LinearLocator(10))
+# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+#
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+#
+#
+# plt.show()
+
+# plt.figure()
+# CS = plt.contour(X, Y, Z)
+# plt.clabel(CS, inline=1, fontsize=10)
+# title='Earth -> Aphosis, ' + str(rev) + ' orbits'
+# plt.title(title)
+#
+# plt.show()
+
+levels = MaxNLocator(nbins=15).tick_values(Z.min(), Z.max())
+cmap = plt.get_cmap('PiYG')
+norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+im = plt.pcolormesh(X, Y, Z, cmap=cmap,vmax=.03)
+plt.colorbar()
+# set the limits of the plot to the limits of the data
+
+title='Earth -> Aphosis, ' + str(rev) + ' orbits'
+plt.title(title)
+
+
 
 plt.show()
