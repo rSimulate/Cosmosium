aaf43b346e26bd473f24f72340d224901e8b3fe7
Tylar Murray <murray.tylar@gmail.com>
2015-08-22 19:17:57 -0400

    closer to working nav, onclick function now trigger (but its messy and parts are missing)

--- directives/frames/cosmosium-frames.js
+++ directives/frames/cosmosium-frames.js
@@ -22,6 +22,169 @@
                 /* Sidebar tree view */
                 $(".sidebar .treeview").tree();
 
+                document.getElementById('asteroidSurveys-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    console.log('asteroid surveys clicked!')
+                    switchContent($('#asteroidSurveys'));
+
+                }, false);
+
+                document.getElementById('dash-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#dash'));
+
+                }, false);
+
+                document.getElementById('fuelNet-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('gov-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('launchpad-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#launchpad'));
+                    launchMap.setSize();
+
+                }, false);
+
+                document.getElementById('launchSys-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('missionControl-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#missionControl'));
+
+                }, false);
+
+                document.getElementById('observatories-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#observatories'));
+                    observatoryMap.setSize();
+
+                }, false);
+
+                document.getElementById('org-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('outreach-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('resMarket-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('research_spaceIndustry-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#research'));
+
+                }, false);
+                document.getElementById('research_humanHabitation-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#research'));
+
+                }, false);
+                document.getElementById('research_robotics-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#research'));
+
+                }, false);
+
+
+
+                document.getElementById('spaceTourism-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('surveyEquip-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#surveyEquip'));
+
+                }, false);
+
+                document.getElementById('systemView-link').addEventListener('click', function (e){
+                    e = e || window.event;
+                    switchContent();
+                }, false);
+
+                document.getElementById('timeline-link').addEventListener('click', function (e){
+                    e = e || window.event;
+
+                    switchContent($('#timeline'));
+
+                }, false);
+
+                document.getElementById('add-object-button').addEventListener('click', function (e) {
+                    e = e || window.event;
+
+                    if (rSimulate) rSimulate.cosmosScene.addTestObject();
+
+
+                }, false);
+
+                document.getElementById('destroy-object-button').addEventListener('click', function (e) {
+                    e = e || window.event;
+
+                    if (rSimulate) rSimulate.cosmosScene.requestRemoveBody();
+
+                }, false);
+
+                document.getElementById('set-target-button').addEventListener('click', function (e) {
+                    e = e || window.event;
+
+                    if (rSimulate) rSimulate.cosmosUI.setCourse(e);
+
+                }, false);
+
+                document.getElementById('plot-course-button').addEventListener('click', function (e) {
+                    e = e || window.event;
+
+                    if (rSimulate) rSimulate.cosmosUI.requestCourse(e);
+
+                }, false);
+
+                document.getElementById('cancel-course-button').addEventListener('click', function (e) {
+                    e = e || window.event;
+
+                    if (rSimulate) rSimulate.cosmosUI.cancelCourse(e);
+
+                }, false);
+
             }]
         };
     });
